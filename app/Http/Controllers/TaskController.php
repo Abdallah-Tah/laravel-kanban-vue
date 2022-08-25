c951a57c91ab7a1d72899db638d86fa0b1bad129
Abdallah Mohamed <abdal_cascad@hotmail.com>
2022-08-24 21:41:36 -0400

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = auth()->user()->statuses()->with('tasks')->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => ['required', 'string', 'max:56'],
+            'description' => ['required', 'string'],
+            'status_id' => ['required', 'exists:statuses,id']
+        ]);
+    
+        return $request->user()
+            ->tasks()
+            ->create($request->only('title', 'description', 'status_id'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+
+    public function sync(Request $request)
+    {
+        $this->validate(request(), [
+            'columns' => ['required', 'array']
+        ]);
+
+        foreach ($request->columns as $status) {
+            foreach ($status['tasks'] as $i => $task) {
+                $order = $i + 1;
+                if ($task['status_id'] !== $status['id'] || $task['order'] !== $order) {
+                    request()->user()->tasks()
+                        ->find($task['id'])
+                        ->update(['status_id' => $status['id'], 'order' => $order]);
+                }
+            }
+        }
+
+        return $request->user()->statuses()->with('tasks')->get();
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('statuses', \App\Http\Controllers\StatusController::class);
+Route::resource('tasks', \App\Http\Controllers\TaskController::class);
+
+Route::put('tasks/sync', 'TaskController@sync')->name('tasks.sync');
+
+
